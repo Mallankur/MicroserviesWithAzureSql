0a1c81715f7b2b914e75f10998583fdad7905aaa
AVINEX9\ankur <ankur@avinex.in>
2023-08-22 15:27:33 +0530

    conflictresolved

---
+++ Src/ReservationApi/Migrations/20230822062934_InitalCreate.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ReservationApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitalCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
